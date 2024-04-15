f0a2cb4dff3bd27bf862241ff1efeaff4e2d1448
Luka Mihajlovic <lukamihgimnazija@gmail.com>
2024-04-15 16:23:56 +0100

    imported map, set up collisions and satapas' doors

---
+++ VR project/Assets/Scripts/flavorText.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class flavorText : MonoBehaviour
+{
+
+    public void NewText(string txt)
+    {
+        Debug.LogWarning(txt);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
